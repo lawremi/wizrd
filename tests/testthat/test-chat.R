dcdf3fc0210cc9db92b1ad7e0aa271098f3c9e99
Michael Lawrence <lawremi@gmail.com>
2024-08-25 17:08:33 -0700

    some more simple tests

--- tests/testthat/test-chat.R
+++ tests/testthat/test-chat.R
@@ -1,7 +1,13 @@
 test_that("calling chat() and friends generates correct responses", {
     model <- stories260K()
 
-##    options(wizrd.debug = 3L)
-    expect_class(chat(model, "Tell me a story"), "Chat")
+    ##    options(wizrd.debug = 3L)
+    chat <- chat(model, "Tell me a story")
+    expect_class(chat, "Chat")
+    expect_length(chat@messages, 3L)
+    chat <- chat(chat, "Tell me another story")
+    expect_class(chat, "Chat")
+    expect_length(chat@messages, 5L)
+    
     expect_string(predict(model, "Tell me a story"))
 })
