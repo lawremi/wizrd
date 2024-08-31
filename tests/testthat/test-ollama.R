c36de2e70053868e866f62e5b4a862d1dbe4b51d
Michael Lawrence <lawremi@gmail.com>
2024-08-31 11:14:46 -0700

    finished basic text-based tests of chat() and predict()

--- tests/testthat/test-ollama.R
+++ tests/testthat/test-ollama.R
@@ -1,13 +1,16 @@
-test_that("calling chat() and friends generates correct responses", {
+test_that("chat() and predict() work for text messages", {
     model <- llama3()
-
-    ##    options(wizrd.debug = 3L)
-    chat <- chat(model, "Tell me a story")
-    expect_class(chat, "Chat")
-    expect_length(chat@messages, 3L)
-    chat <- chat(chat, "Tell me another story")
-    expect_class(chat, "Chat")
-    expect_length(chat@messages, 5L)
+    model@instructions <- "Respond with a single sentence"
     
-    expect_string(predict(model, "Tell me a story"))
+    ##    options(wizrd.debug = 3L)
+    chat <- chat(model, "Who created R?")
+    expect_length(chat@messages, 3L)
+    expect_match(last_output(chat), "Robert.*Ross|Ross.*Robert")
+
+    chat2 <- chat(chat, "When did they do it?")
+    expect_length(chat2@messages, 5L)
+    expect_match(last_output(chat2), "1992")
+    
+    expect_match(predict(model, "Who created R?"), "Robert.*Ross|Ross.*Robert")
+    expect_match(predict(chat, "When did they do it?"), "1992")
 })
