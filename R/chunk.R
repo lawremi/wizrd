45c8f2103451df2b68865d17100416f09fc2c83d
Michael Lawrence <lawremi@gmail.com>
2024-10-17 14:42:56 -0700

    fix up Rd chunking

--- R/chunk.R
+++ R/chunk.R
@@ -4,10 +4,11 @@ chunk_Rd <- function(package) {
 
 chunk_Rd_db <- function(x) {
     chunks <- lapply(x, chunk_Rd_file)
-    aliases <- lapply(x, \(f) paste(Rd_aliases(f), collapse = ","))
-    section <- unlist(lapply(chunks, names))
-    data.frame(text = unlist(chunks),
-               aliases = rep(aliases, lengths(chunks)),
+    aliases <- vapply(x, \(f) paste(Rd_aliases(f), collapse = ","),
+                      character(1L))
+    section <- unlist(lapply(chunks, names), use.names = FALSE)
+    data.frame(text = unlist(chunks, use.names = FALSE),
+               aliases = rep(unname(aliases), lengths(chunks)),
                section = section)
 }
 
@@ -15,10 +16,12 @@ Rd_sections <- c("description", "usage", "arguments", "details", "section",
                  "value", "examples")
 
 chunk_Rd_file <- function(x) {
-    names(x) <- vapply(x, attr, character(1L), "Rd_tag")
-    c(file = Rd_src(x), vapply(x[Rd_sections], Rd_src, character(1L)))
+    names(x) <- gsub("\\", "", vapply(x, attr, character(1L), "Rd_tag"),
+                     fixed = TRUE)
+    c(file = Rd_src(x), vapply(x[names(x) %in% Rd_sections], Rd_src,
+                               character(1L)))
 }
 
 Rd_aliases <- function(x) {
-    unlist(Filter(\(xi) attr(xi, "Rd_tag") == "alias"))
+    unlist(Filter(\(xi) attr(xi, "Rd_tag") == "\\alias", x), use.names = FALSE)
 }
