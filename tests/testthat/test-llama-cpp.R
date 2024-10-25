b04f6e898b70244122837e277b3d24c8483002e0
Michael Lawrence <lawremi@gmail.com>
2024-10-25 09:39:07 -0700

    clean up model construction in llama.cpp tests

--- tests/testthat/test-llama-cpp.R
+++ tests/testthat/test-llama-cpp.R
@@ -1,13 +1,12 @@
 test_that("calling chat() and friends generates correct responses", {
-    model <- llama3()
+    model <- llama_cpp_model_from_ollama(llama3()@name)
 
-    model <- llama_cpp_model_from_ollama(model@name)
     chat <- chat(model, "How was R created, in three sentences?")
 })
 
 test_that("embedding works", {
-    model <- llama_cpp_model_from_ollama("nomic-embed-text:v1.5",
-                                         mode = "embedding")
+    model <- llama_cpp_model_from_ollama(nomic()@name, mode = "embedding")
+    
     embedding <- embed_text(model, "test")
     expect_equal(dim(embedding), c(1L, 768L))
 }
