0b824a8ee215fe57e189ca8efc74dd540b996446
Michael Lawrence <lawremi@gmail.com>
2024-12-13 13:12:48 -0800

    calling a model as a tool seems to work again

--- tests/testthat/test-tools.R
+++ tests/testthat/test-tools.R
@@ -1,13 +1,13 @@
-test_that("We can create a simple agent", {
+test_that("We can make a model callable as a tool", {
     model <- openai_model() |> accept_as("Who created {language}?") |>
         output_as(data.frame(first_name = character(), last_name = character()))
-    agt <- agentize(model)
+    agt <- convert(model, wizrd:::Tool)
     ans <- agt(list(language = "R"))
     expect_contains(ans$first_name, "Robert")
 
     var <- 1:3
     meanie := llama3() |> equip(tool(mean) |> can_accept_as(x = class_name)) |>
-        instruct("Compute the mean of a variable") |> agentize()
+        instruct("Compute the mean of a variable")
     ans <- openai_model() |>
         equip(meanie, instructions = "Call to find the mean of a variable") |>
         predict("What is the mean of the variable 'var'?")
