868efe824e8edcac5b4cc4c0f0f9efad34550fd8
Michael Lawrence <lawremi@gmail.com>
2025-01-24 14:42:31 -0800

    improved structured output tests

--- tests/testthat/test-formats.R
+++ tests/testthat/test-formats.R
@@ -20,13 +20,24 @@ test_that("structured output works", {
     ))
     model <- output_as(model, wizrd:::new_list_property(of = Person))
     ans <- predict(model, "Who created R?")
-    expect_contains(ans[[1L]]@first_name, "Robert|Ross")    
+    expect_contains(ans[[1L]]@first_name, "Robert|Ross")
+
+    model <- output_as(model, list()) # unconstrained JSON output
+    ans <- predict(model, "Who created R?")
+    expect_is(ans, "list")
+    expect_contains(unlist(ans), "Robert|Ross")
 })
 
 test_that("glue templating works", {
     ans <- llama() |>
-        accept_as("Output a {nchar}-letter word starting with '{initial}'.") |>
+        prompt_as("Output a {nchar}-letter word starting with '{initial}'.") |>
         predict(list(nchar = 4L, initial = "a"))
     expect_equal(ans, "Able")
+
+    ans <- llama() |>
+        system_prompt_as("Answer questions about {language}") |>
+        chat(system_params = list(language = "R")) |>
+        predict("Who created the language?")
+    expect_contains(ans, "Robert|Ross")
 })
 
