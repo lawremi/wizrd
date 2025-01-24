94b8d7c6af318c6c281e5e96653b86ce69a2e292
Michael Lawrence <lawremi@gmail.com>
2025-01-24 14:37:41 -0800

    introduce a Parameters parent for the dynamic parameter classes, currently just for pretty printing

--- R/ToolSignature.R
+++ R/ToolSignature.R
@@ -1,15 +1,20 @@
+Parameters := new_class(abstract = TRUE)
+
+new_parameters <- function(x = list()) {
+    new_class("parameters", Parameters, properties = dodge_dots(x))
+}
+
 ToolSignature := new_class(
     properties = list(
         parameters = new_property(
             S7_class,
             setter = \(self, value) {
                 if (is.list(value))
-                    value <- new_class("parameters",
-                                       properties = dodge_dots(value))
+                    value <- new_parameters(value)
                 self@parameters <- value
                 self
             },
-            default = new_class("parameters")
+            default = quote(new_parameters())
         ),
         value = union_classes
     ))
@@ -26,4 +31,20 @@ dodge_dots <- function(x) {
     if (!is.null(x))
         names(x)[names(x) == "..."] <- "_dots"
     x
+}
+
+Parameters_as_call <- function(x) {
+    as.call(c(quote(alist), expand_dots(props(x))))
+}
+
+## Does not help printing from inside a data.frame, because
+## format.default() delegates specifically to format.default() when
+## looping over a list (column).
+## It should probably call the generic instead.
+method(format, Parameters) <- function(x, ...) {
+    format(Parameters_as_call(x), ...)
+}
+
+method(print, Parameters) <- function(x, ...) {
+    print(Parameters_as_call(x), ...)
 }
