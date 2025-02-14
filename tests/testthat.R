c5c6a6cc00dc7c69391abf3444b1d1ead4c089a8
Michael Lawrence <lawremi@gmail.com>
2025-02-14 15:16:34 -0800

    start playing around with http caching of tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,15 +1,11 @@
-# This file is part of the standard setup for testthat.
-# It is recommended that you do not modify it.
-#
-# Where should you do additional test configuration?
-# Learn more about the roles of various files in:
-# * https://r-pkgs.org/tests.html
-# * https://testthat.r-lib.org/reference/test_package.html#special-files
-
 library(testthat)
 library(S7)
 library(wizrd)
 
 `:=` <- wizrd:::`:=`
 
-test_check("wizrd")
+setwd('tests')
+
+httptest2::with_mock_dir("http", {
+    test_check("wizrd")
+})
