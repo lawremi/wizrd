9b19e162300806c3082e836608e4791088ad303e
Michael Lawrence <michafla@gene.com>
2025-04-19 04:04:26 -0700

    thanks to MCP changes, constrained outputs from langsmith can be formal objects

--- tests/testthat/test-langsmith.R
+++ tests/testthat/test-langsmith.R
@@ -9,5 +9,5 @@ test_that("Langsmith templates work", {
     model <- llama(server) |> prompt_as("vectrix/intent_detection")
     expect_match(model@io@input@template, "intent")
     ans <- model |> predict(list(chat_history = "", question = "Hello?"))
-    expect_equal(ans$intent, "greeting")
+    expect_equal(ans@intent, "greeting")
 })
