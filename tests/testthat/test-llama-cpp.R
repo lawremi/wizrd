47f578c916d443959e1650a61b0bf6db3f53eb6f
Michael Lawrence <michafla@gene.com>
2025-04-19 17:21:25 -0700

    use zero temperature

--- tests/testthat/test-llama-cpp.R
+++ tests/testthat/test-llama-cpp.R
@@ -6,7 +6,8 @@ test_that("llama.cpp chatting works with a llamafile", {
 
 test_that("llama.cpp supports external weights and generating embeddings", {
     model <- wizrd:::llama_cpp_model_from_ollama(nomic(server)@name,
-                                                 mode = "embedding")
+                                                 mode = "embedding",
+                                                 temperature = 0)
     embedding <- embed_text(model, "test")
     expect_equal(dim(embedding), c(1L, 768L))
 })
